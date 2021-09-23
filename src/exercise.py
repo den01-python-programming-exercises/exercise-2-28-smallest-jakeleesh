d05a41e4c06de5b1f50d537a7dd705127d493556
Jake Lee <jake.lee@fdmgroup.com>
2021-09-23 18:26:48 +0800

    Attempts solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,15 @@
 def main():
     #write your code below this line
+    num1 = int(input())
+    num2 = int(input())
+    answer = smallest(num1, num2)
+    print("Smallest: " + str(answer))
+
+def smallest(num1, num2):
+    if (num1 < num2):
+        return num1
+    else:
+        return num2
 
 if __name__ == '__main__':
     main()
